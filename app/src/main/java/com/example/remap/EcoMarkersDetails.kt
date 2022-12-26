c78a88d2e8f37d16df31adc860fd559217d05b6b
Sergey Gnatenko <gnaten.ko2004@gmail.com>
2022-12-26 15:58:00 +0300

    Теперь в "Маркировках" появилась информация конкретной маркировки и картинки. Убрал ползунок у ScrollView.

--- app/src/main/java/com/example/remap/EcoMarkersDetails.kt
+++ app/src/main/java/com/example/remap/EcoMarkersDetails.kt
@@ -1,14 +1,44 @@
 package com.example.remap
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.remap.models.EcoMarkers
+import org.w3c.dom.Text
 
 class EcoMarkersDetails : AppCompatActivity() {
+
+    lateinit var ecoMarkerImage: ImageView
+    lateinit var ecoMarkerTitle: TextView
+    lateinit var ecoMarkerExamplesTitle: TextView
+    lateinit var ecoMarkerExamples: TextView
+    lateinit var ecoMarkerDescription: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getSupportActionBar()?.hide();
         setContentView(R.layout.activity_eco_markers_details)
 
-        val description = intent.getStringExtra("MARKER")
+        val description = intent.getParcelableExtra<EcoMarkers>("MARKER")
+
+        ecoMarkerImage = findViewById(R.id.ecoMarkerImage)
+        ecoMarkerImage.setImageResource(description!!.contentImage)
+
+        ecoMarkerTitle = findViewById(R.id.ecoMarkerTitle)
+        ecoMarkerTitle.text = description.title
+
+        ecoMarkerExamplesTitle = findViewById(R.id.ecoMarkerExampleTitle)
+        ecoMarkerExamplesTitle.text = description.examplesTitle
+
+        ecoMarkerExamples = findViewById(R.id.ecoMarkerExamples)
+        ecoMarkerExamples.text = description.examples
+
+        ecoMarkerDescription = findViewById(R.id.ecoMarkerDescription)
+        ecoMarkerDescription.text = description.description
+
     }
 }
--- app/src/main/java/com/example/remap/models/EcoMarkers.kt
+++ app/src/main/java/com/example/remap/models/EcoMarkers.kt
@@ -1,5 +1,39 @@
 package com.example.remap.models
 
-import android.app.ActivityManager
+import android.os.Parcel
+import android.os.Parcelable
 
-data class EcoMarkers(var imageResource: Int, var title: String, var description: String)
+data class EcoMarkers(var imageResource: Int, var contentImage: Int, var title: String, var examplesTitle: String, var examples: String, var description: String): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(imageResource)
+        parcel.writeInt(contentImage)
+        parcel.writeString(title)
+        parcel.writeString(examplesTitle)
+        parcel.writeString(examples)
+        parcel.writeString(description)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<EcoMarkers> {
+        override fun createFromParcel(parcel: Parcel): EcoMarkers {
+            return EcoMarkers(parcel)
+        }
+
+        override fun newArray(size: Int): Array<EcoMarkers?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
